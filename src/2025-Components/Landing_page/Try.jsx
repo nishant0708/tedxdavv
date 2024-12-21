04211ba798965bb5f17b98472c3dc35b8f9d5921
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-12-22 02:44:19 +0530

    Wait

---
+++ src/2025-Components/Landing_page/Try.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Landing_page.css"
+function Try() {
+  return (
+    <div className='try'>
+     <p>Hello</p>
+        </div>
+  )
+}
+
+export default Try
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -5,6 +5,7 @@ import Speakerf from '../nik-pages/Speakerf'
 
 import FaqPage from '../../pages/questions/faqSection'
 import Landing_page from '../../2025-Components/Landing_page/Landing_page'
+import Try from '../../2025-Components/Landing_page/Try'
 
 
 // import Mehnat from "../jay mc/Mehnat";
@@ -14,8 +15,11 @@ const Main = () => {
   return (
     <div>
        <Landing_page/>
-        <Speakerf/>
-        <FaqPage/>
+       <Try/>
+       <Speakerf/>
+       <FaqPage/>
+    
+        
     
     </div>
   )
